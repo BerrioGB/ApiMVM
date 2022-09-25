3ca4f72792904c72ac72da95f412371a6ce0c2a8
Juan Carlos Gómez Berrio <juan8522011@gmail.com>
2022-09-24 23:42:12 -0500

    Dev-Api-FirstAdvance

---
+++ api_rest/src/database/connection.js
@@ -0,0 +1,22 @@
+import sql from 'mssql'
+
+const dbSettings = {
+    user: 'jovenesmvm',
+    password: '0^618Bjt37y@',
+    server: 'gestionsolicitudes.database.windows.net' , 
+    database:'gestionsolicitudes',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true, 
+    }
+}
+
+export async function getConnection()
+{
+    try {
+        const pool = await sql.connect(dbSettings);
+        return pool;    
+    } catch (error) {
+        console.error(error);
+    }
+}
