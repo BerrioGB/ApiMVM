56494077f21d660dd8b776700c6b0830b1916d1c
Juan Carlos Gómez Berrio <juan8522011@gmail.com>
2022-10-21 16:00:45 -0500

    Change_Database

--- api_rest/src/app.js
+++ api_rest/src/app.js
@@ -1,17 +1,20 @@
 import express from "express";
-import config from './config'
-import productsRoutes from './routes/products.routes'
-
+import config from "./config";
+import productsRoutes from "./routes/products.routes";
 
 const app = express();
 
-
 //configuraciones
-app.set('port', config.port)
+app.set("port", config.port);
 
 //middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-app.use(productsRoutes)
-export default app
+app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+app.use(productsRoutes);
+export default app;
--- api_rest/src/database/connection.js
+++ api_rest/src/database/connection.js
@@ -1,24 +1,23 @@
-import sql from 'mssql'
+import sql from "mssql";
 
 const dbSettings = {
-    user: 'jovenesmvm',
-    password: '0^618Bjt37y@',
-    server: 'gestionsolicitudes.database.windows.net' , 
-    database:'gestionsolicitudes',
-    options: {
-        encrypt: true,
-        trustServerCertificate: true, 
-    }
+  user: "jovenesmvm",
+  password: "0^618Bjt37y@",
+  server: "sql-gestionsolicitudes-pdn-001.database.windows.net",
+  database: "sqldb-gestionsolicitudes-pdn-001",
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+
+export async function getConnection() {
+  try {
+    const pool = await sql.connect(dbSettings);
+    return pool;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export async function getConnection()
-{
-    try {
-        const pool = await sql.connect(dbSettings);
-        return pool;    
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-export{sql};
+export { sql };
